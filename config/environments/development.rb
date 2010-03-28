6c51a764124b011fdaf48697cd7506fe8688e970
Adrian Smith <adrian.smith@ennova.com.au>
2010-03-29 06:15:10 +1000

    Updated .gems

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,5 +23,5 @@ config.gem 'flog'
 config.gem 'flay'
 config.gem 'roodi'
 config.gem 'rcov'
-config.gem "rspec", :lib => false, :version => ">= 1.2.0"
-config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0"
+config.gem "rspec", :lib => false, :version => "1.2.0"
+config.gem "rspec-rails", :lib => false, :version => "1.2.0"
