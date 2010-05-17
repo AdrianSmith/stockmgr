f21c5616338af0821696ddea6ef3957a57a2e923
Adrian Smith <adrian.smith@ennova.com.au>
2010-05-17 10:05:55 +1000

    fixed tests

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,90 +1,53 @@
 class PaymentsController < ApplicationController
-  # GET /payments
-  # GET /payments.xml
+
   def index
     @payments = Payment.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @payments }
-    end
   end
 
-  # GET /payments/1
-  # GET /payments/1.xml
   def show
     @payment = Payment.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @payment }
-    end
   end
 
-  # GET /payments/new
-  # GET /payments/new.xml
   def new
     @payment = Payment.new
     @customers = User.customers.map{|t| [t.pretty_name, t.id]}
     @payment_types = PaymentType.find(:all).map{|t| [t.name.titleize, t.id]}
-    
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @payment }
-    end
   end
 
-  # GET /payments/1/edit
   def edit
     @payment = Payment.find(params[:id])
     @customers = User.customers.map{|t| [t.pretty_name, t.id]}
     @payment_types = PaymentType.find(:all).map{|t| [t.name.titleize, t.id]}
   end
 
-  # POST /payments
-  # POST /payments.xml
   def create
     @payment = Payment.new(params[:payment])
+    @customers = User.customers.map{|t| [t.pretty_name, t.id]}
+    @payment_types = PaymentType.find(:all).map{|t| [t.name.titleize, t.id]}
 
-    respond_to do |format|
-      if @payment.save
-        flash[:notice] = 'Payment was successfully created.'
-        format.html { redirect_to(@payment.user) }
-        format.xml  { render :xml => @payment, :status => :created, :location => @payment }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @payment.errors, :status => :unprocessable_entity }
-      end
+    if @payment.save
+      flash[:notice] = 'Payment was successfully created.'
+      redirect_to(@payment.user)
+    else
+      render :action => "new"
     end
   end
 
-  # PUT /payments/1
-  # PUT /payments/1.xml
   def update
     @payment = Payment.find(params[:id])
 
-    respond_to do |format|
-      if @payment.update_attributes(params[:payment])
-        flash[:notice] = 'Payment was successfully updated.'
-        format.html { redirect_to(@payment) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @payment.errors, :status => :unprocessable_entity }
-      end
+    if @payment.update_attributes(params[:payment])
+      flash[:notice] = 'Payment was successfully updated.'
+      redirect_to(@payment)
+    else
+      render :action => "edit"
     end
   end
 
-  # DELETE /payments/1
-  # DELETE /payments/1.xml
   def destroy
     @payment = Payment.find(params[:id])
     user = @payment.user
     @payment.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(user) }
-      format.xml  { head :ok }
-    end
+    redirect_to(user)
   end
 end
