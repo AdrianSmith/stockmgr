98d822dd65dc87dd0d98c24e3dfbe1774655b6f5
Adrian Smith <adrian.smith@ennova.com.au>
2010-08-15 12:32:08 +1000

    Updated gems

--- config/environment.rb
+++ config/environment.rb
@@ -20,8 +20,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"  
 
-  config.gem 'authlogic', :source => "http://gemcutter.org", :version => '2.1.3'
-  config.gem 'prawn', :version => '0.8.4' 
+  config.gem 'authlogic'
+  config.gem 'prawn'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
