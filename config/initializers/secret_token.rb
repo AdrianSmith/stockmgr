4277ef27bc56f8ad2f93e0ff2f4868cd2deba7e2
Adrian Smith <adrian.smith@ennova.com.au>
2012-06-23 18:39:05 +1000

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prodmgr::Application.config.secret_token = '1d61bb574cb518e945b260be2308d9e153379bd27b5818dd4d7be0010e0e5bd92b02c049908ea7748dc26d510c4223cb6e0d25938a186a95120ec877da4c02bb'
