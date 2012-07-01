e71d50854960c978e80629ad77adba1706e96b5d
Adrian Smith <adrian.smith@ennova.com.au>
2012-07-01 16:50:22 +1000

    Clean payment type views

--- app/controllers/product_types_controller.rb
+++ app/controllers/product_types_controller.rb
@@ -1,4 +1,10 @@
 class ProductTypesController < InheritedResources::Base
   before_filter :authenticate_user!
   actions :all, :except => [ :destroy ]
+
+  def show
+    @related_products = Product.where(:product_type_id => params[:id])
+    show!
+  end
+
 end
