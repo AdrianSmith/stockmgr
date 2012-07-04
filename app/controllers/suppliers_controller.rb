9ddebadc3a9ee27fc0259647bcd378965a29ef9e
Adrian Smith <adrian.smith@ennova.com.au>
2012-07-04 19:17:12 +1000

    Fixed Supplier views

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,83 +1,26 @@
-class SuppliersController < ApplicationController
-  # GET /suppliers
-  # GET /suppliers.json
+class SuppliersController < InheritedResources::Base
+  before_filter :authenticate_user!
+  before_filter :find_order, :only => [:toggle_order_received_status, :toggle_order_paid_status]
+
   def index
-    @suppliers = Supplier.all
+    @suppliers = Supplier.order('name ASC').all
+  end
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @suppliers }
+  def toggle_order_received_status
+    @order.received = !@order.received?
+    @order.paid = true if @order.received?
+    @order.save!
+    redirect_to(:controller => 'suppliers', :action => 'show', :id => params[:id])
+  end
+
+  def toggle_order_paid_status
+    @order.paid = !@order.paid?
+    @order.save!
+    redirect_to(:controller => 'suppliers', :action => 'show', :id => params[:id])
+  end
+
+  protected
+    def find_order
+      @order = PurchaseOrder.find(params[:order_id])
     end
-  end
-
-  # GET /suppliers/1
-  # GET /suppliers/1.json
-  def show
-    @supplier = Supplier.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @supplier }
-    end
-  end
-
-  # GET /suppliers/new
-  # GET /suppliers/new.json
-  def new
-    @supplier = Supplier.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @supplier }
-    end
-  end
-
-  # GET /suppliers/1/edit
-  def edit
-    @supplier = Supplier.find(params[:id])
-  end
-
-  # POST /suppliers
-  # POST /suppliers.json
-  def create
-    @supplier = Supplier.new(params[:supplier])
-
-    respond_to do |format|
-      if @supplier.save
-        format.html { redirect_to @supplier, notice: 'Supplier was successfully created.' }
-        format.json { render json: @supplier, status: :created, location: @supplier }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @supplier.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /suppliers/1
-  # PUT /suppliers/1.json
-  def update
-    @supplier = Supplier.find(params[:id])
-
-    respond_to do |format|
-      if @supplier.update_attributes(params[:supplier])
-        format.html { redirect_to @supplier, notice: 'Supplier was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @supplier.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /suppliers/1
-  # DELETE /suppliers/1.json
-  def destroy
-    @supplier = Supplier.find(params[:id])
-    @supplier.destroy
-
-    respond_to do |format|
-      format.html { redirect_to suppliers_url }
-      format.json { head :no_content }
-    end
-  end
 end
