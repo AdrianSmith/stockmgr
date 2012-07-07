56f57a256eda483ce9a7ee2da8c7ab68db7dbec7
Adrian Smith <adrian.smith@ennova.com.au>
2012-07-07 19:21:13 +1000

    Added format helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,4 +34,82 @@ module ApplicationHelper
   def format_time(time)
     time.strftime("%H:%M") if time
   end
+
+  def format_boolean(status)
+    if status
+      icon_tick
+    else
+      icon_cross
+    end
+  end
+
+  def icon_csv(alt_text='CSV File')
+    build_image_tag("csv48x48.jpg", alt_text)
+  end
+
+  def icon_html(alt_text='HTML File')
+    build_image_tag("html35x35.jpg", alt_text)
+  end
+
+  def icon_pdf(alt_text='PDF File')
+    build_image_tag("pdf48x48.jpg", alt_text)
+  end
+
+  def icon_tick(alt_text='Tick')
+    build_image_tag("icons/001_06.png", alt_text)
+  end
+
+  def icon_cross(alt_text='Cross')
+    build_image_tag("icons/001_05.png", alt_text)
+  end
+
+  def icon_stop(alt_text='Stop')
+    build_image_tag("icons/001_29.png", alt_text)
+  end
+
+  def icon_exclaim(alt_text='Exclaim')
+    build_image_tag("icons/001_11.png", alt_text)
+  end
+
+  def icon_warning(alt_text='Warning')
+    build_image_tag("icons/001_30.png", alt_text)
+  end
+
+  def icon_add(alt_text='Add')
+    build_image_tag("icons/001_01.png", alt_text)
+  end
+
+  def icon_subtract(alt_text='Subtract')
+    build_image_tag("icons/001_02.png", alt_text)
+  end
+
+  def icon_play(alt_text='Play')
+    build_image_tag("icons/001_25.png", alt_text)
+  end
+
+  def icon_show(alt_text='Show')
+    build_image_tag("icons/001_37.png", alt_text)
+  end
+
+  def icon_edit(alt_text='Edit')
+    build_image_tag("icons/001_45.png", alt_text)
+  end
+
+  def icon_delete(alt_text='Delete')
+    build_image_tag("icons/001_49.png", alt_text)
+  end
+
+  def icon_ok(alt_text='OK')
+    build_image_tag("icons/001_18.png", alt_text)
+  end
+
+  def icon_not_ok(alt_text='NOT OK')
+    build_image_tag("icons/001_19.png", alt_text)
+  end
+
+  private
+
+  def build_image_tag(image_file, alt_text)
+    image_tag(image_file, :size =>"20x20", :alt => alt_text)
+  end
 end
