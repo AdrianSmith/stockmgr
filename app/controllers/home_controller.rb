c6bc767dd869ee79c3212e17fd8e871976d5684e
Adrian Smith <adrian.smith@ennova.com.au>
2012-07-07 19:16:34 +1000

    Enhance home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
+    @number_of_customers = Customer.count
+    @number_of_products = Product.count
+
+    @unpaid_sales_orders = SalesOrder.where(:paid => false).order('invoiced_at DESC').limit(5)
+
+    @total_sales = SalesOrder.all.inject(0){|sum, o| sum + o.total_price}
+    @total_payments = Payment.all.inject(0){|sum, o| sum + o.amount}
+    @balance = @total_payments - @total_sales
   end
 end
