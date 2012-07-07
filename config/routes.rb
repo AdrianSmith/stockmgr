398087e1a39dff9936fdbae4796d0683b6b74c9e
Adrian Smith <adrian.smith@ennova.com.au>
2012-07-07 19:18:15 +1000

    Added missing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Prodmgr::Application.routes.draw do
 
-  resources :suppliers
-
-  resources :sales_order_items
-
-  resources :sales_orders
-
   resources :products
 
   resources :product_types
@@ -16,9 +10,27 @@ Prodmgr::Application.routes.draw do
 
   resources :payment_types
 
-  resources :customers
+  resources :customers do
+    member do
+      put 'toggle_order_invoiced_status'
+      put 'toggle_order_paid_status'
+    end
+  end
 
   devise_for :users
+
+  resources :sales_orders do
+    member do
+      get 'invoice_pdf'
+      put 'add_basket_item'
+      put 'add_sales_order_item'
+      delete 'remove_basket_item'
+      delete 'remove_sales_order_item'
+    end
+  end
+  resources :sales_order_items
+
+  resources :suppliers
 
   root :to => 'home#index'
 end
